c26c342320a799eba93101e74f90d3b8cd596009
WhiColMi <mr.whicolmi@gmail.com>
2025-08-27 17:28:10 +0200

    leader t for toggle term

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -2,6 +2,7 @@ return {
   "akinsho/toggleterm.nvim",
   config = function()
     require("toggleterm").setup{}
+    vim.api.nvim_set_keymap("n", "<leader>t", "<cmd>ToggleTerm<CR>", {noremap = true, silent = true})
   end
 }
 
