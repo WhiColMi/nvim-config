1a5c8197549e2cc2b263e2de2e6de3025a3ae34a
WhiColMi <mr.whicolmi@gmail.com>
2025-10-15 15:58:46 +0200

    added option to set root up by one directory by 'n' in neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -14,5 +14,11 @@ return {
       }
     })
     vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal right<CR>', {})
+    vim.keymap.set('n', ',', function()
+      local cwd = vim.fn.getcwd()                      -- current working dir
+      local parent = vim.fn.fnamemodify(cwd, ":h")    -- parent directory
+      vim.cmd("cd " .. parent)                         -- change cwd
+      vim.notify("CWD: " .. parent)
+    end, { noremap = true, silent = true })
   end
 }
