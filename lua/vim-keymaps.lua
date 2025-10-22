e722b83683e21b771cc5f26f1bb4ac8737b6d780
WhiColMi <mr.whicolmi@gmail.com>
2025-10-22 22:54:55 +0200

    navigation

--- lua/vim-keymaps.lua
+++ lua/vim-keymaps.lua
@@ -1,4 +1,21 @@
-vim.keymap.set('n', '<A-h>', '<C-w><')
-vim.keymap.set('n', '<A-l>', '<C-w>>')
-vim.keymap.set('n', '<A-j>', '<C-w>-')
-vim.keymap.set('n', '<A-k>', '<C-w>+')
+vim.g.mapleader = ' '
+
+local km = vim.keymap
+local opts = { silent = true, noremap = true }
+
+km.set('n', '<leader>h', '2<cmd>:vertical resize -2<CR>', opts)
+km.set('n', '<leader>l', '2<cmd>:vertical resize +2<CR>', opts)
+km.set('n', '<leader>j', '2<cmd>:resize +2<CR>', opts)
+km.set('n', '<leader>k', '2<cmd>:resize -2<CR>', opts)
+
+-- navigation
+vim.g.tmux_navigator_no_mappings = 1
+
+km.set('n', '<C-h>', ':TmuxNavigateLeft<CR>', opts)
+km.set('n', '<C-j>', ':TmuxNavigateDown<CR>', opts)
+km.set('n', '<C-k>', ':TmuxNavigateUp<CR>', opts)
+km.set('n', '<C-l>', ':TmuxNavigateRight<CR>', opts)
+
+km.set('n', '<leader>|', ':vsplit<CR>', opts)
+km.set('n', '<leader>-', ':split<CR>', opts)
+km.set('n', '<leader>x', ':close<CR>', opts)
