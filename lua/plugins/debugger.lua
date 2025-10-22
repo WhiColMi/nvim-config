a6c43cb6a556e3b6f9643b5e9f8144b82bf2127d
WhiColMi <mr.whicolmi@gmail.com>
2025-10-22 20:58:44 +0200

    debugger fix, missing dependency

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -7,6 +7,7 @@
 return {
   "mfussenegger/nvim-dap",
   dependencies = {
+    "nvim-neotest/nvim-nio",
     "rcarriga/nvim-dap-ui",
     "theHamsta/nvim-dap-virtual-text",
     "mfussenegger/nvim-dap-python",
